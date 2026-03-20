ce65ffba9665fd7f8ddbd30c946ef63768547e7e
zerotheking152-png <zerotheking152@gmail.com>
2026-03-21 03:14:15 +0700

    Update hamzHub.lua

--- hamzHub.lua
+++ hamzHub.lua
@@ -8,6 +8,7 @@ local CleanupCast=FS:WaitForChild("CleanupCast")
 local FishGiver=FS:WaitForChild("FishGiver")
 local ReplicatePullAlert=FS:WaitForChild("ReplicatePullAlert")
 local CastReplication=FS:WaitForChild("CastReplication")
+local PrecalcFish=FS:WaitForChild("PrecalcFish")
 local running=false
 Tab:CreateToggle({Name="Blati",CurrentValue=false,Flag="Blati",Callback=function(v)
 running=v
@@ -16,6 +17,7 @@ while running do
 CleanupCast:FireServer()
 CastReplication:FireServer(Vector3.new(-121.75434875488281,22.01178741455078,309.1944580078125),Vector3.new(-11.061860084533691,5,-11.56007194519043),"Basic Rod",13.940870761871338)
 ReplicatePullAlert:FireServer("rbxassetid://76503247176490")
+PrecalcFish:InvokeServer()
 FishGiver:FireServer({hookPosition=Vector3.new(-126.91644287109375,3.4312500953674316,303.80047607421875)})
 task.wait(0.03)
 end
